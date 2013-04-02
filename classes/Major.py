4c4211ae93a59a5e8f24e9b06389673fe06ebc79
Kemal Segizbayev <kemal.segizbayev@gmail.com>
2013-04-02 10:42:24 -0700

    Created User and Major classes

--- classes/Major.py
+++ classes/Major.py
@@ -1,1 +1,9 @@
-#The Major class will go here
+class Major():
+    def _init_(self, name, school, assist_code):
+        self.name = name
+        self.school = school
+        self.assist_code = assist_code
+        self.classes_required = []
+    def add_required_classes(classes):
+        self.classes_required += classes 
+        
--- classes/User.py
+++ classes/User.py
@@ -1,1 +1,8 @@
-#The User class will go here
+class User():
+    def _init_(self, name, password):
+        self.name = name
+        self.password = password
+        self.majors = []
+        self.classes_taken = []
+    def add_desired_major(major):
+        self.append(major)
